b3bb2961eb73008046a7f94736702d1e9b198a06
deontic <68165727+deontic@users.noreply.github.com>
2022-09-11 04:18:27 +0300

    reduce filesize
    don't include `<codecvt>`

--- src/context-path.cpp
+++ src/context-path.cpp
@@ -1,39 +1,38 @@
-﻿// context-path.cpp
+// context-path.cpp
 // clang, ISO C++20
-// adds option "add current folder to PATH" to context menu
+// adds options allowing the addition and removal of a folder from PATH
+// to the context menu
 
+#include <atlbase.h>
+#include <atlconv.h>
 #include <windows.h>
 
-#include <codecvt>
 #include <iostream>
-#include <string_view>
-
-using std::cin;
-using std::cout;
-using std::endl;
-
 // linker -> manifest file -> UAC Execution level -> administrator could be used
 // to force running as admin but this would run every time we use it from the
 // context menu as well note that it won't add the context menu entry unless exe
 // is run as administrator
+
+HKEY hkey;
+
 void logPause(std::string_view msg) {
-  cout << msg << endl;
+  std::cout << msg << std::endl;
   system("pause > nul");
 }
 
 // https://docs.microsoft.com/en-us/windows/win32/api/winreg/
 
-bool createKey(HKEY&& hkey, LPCWSTR&& lpSubKey, HKEY& phkResult) {
+bool createKey(HKEY&& hkey, const LPCWSTR& lpSubKey, HKEY& phkResult) {
   return RegCreateKeyW(hkey, lpSubKey, &phkResult) == ERROR_SUCCESS;
 };
 
-bool openKey(HKEY&& hkey, LPCWSTR&& lpSubKey, REGSAM&& samDesired,
+bool openKey(HKEY&& hkey, const LPCWSTR& lpSubKey, REGSAM&& samDesired,
              HKEY& phkResult) {
   return RegOpenKeyEx(hkey, lpSubKey, 0, samDesired, &phkResult) ==
          ERROR_SUCCESS;
 }
 
-bool setValue(HKEY&& hkey, const LPCSTR&& lpSubKey, DWORD&& dwType,
+bool setValue(HKEY&& hkey, const LPCSTR& lpSubKey, DWORD&& dwType,
               LPSTR lpData) {
   return RegSetValueA(hkey, lpSubKey, dwType, lpData, sizeof(lpData)) ==
          ERROR_SUCCESS;
@@ -50,15 +49,14 @@ data.size() + 1) == ERROR_SUCCESS;
 }
 */
 
-HKEY hkey;
-std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-
 int main(int argc, char* argv[]) {
-  // indexing argv[1] when it doesn't exist causes an access violation error
-  if (argc == 2) {
-    std::string path = "Environment";
+  // indexing argv[1] when it doesn't exist should cause an access violation
+  // error
+  if (argc > 1) {
+    ShowWindow(::GetConsoleWindow(), SW_HIDE);
     openKey(HKEY_CURRENT_USER, L"Environment", KEY_ALL_ACCESS, hkey);
 
+    // TODO add getPath function
     /*   If hKey specifies HKEY_PERFORMANCE_DATA and the pvData buffer
            is not large enough to contain all of the returned data,
            the function returns ERROR_MORE_DATA and
@@ -73,68 +71,101 @@ int main(int argc, char* argv[]) {
     // yeah but 50 K should be enough I don't care if I'm allocating too
     // much, I can't be bothered to do all the above and if you can, you
     // should ⬇️⬇️⬇️
-
     char val[50000];
     DWORD dataSize = sizeof(val);
     if (ERROR_SUCCESS == RegGetValueA(HKEY_CURRENT_USER, "Environment", "Path",
                                       RRF_RT_ANY, nullptr, &val, &dataSize)) {
       std::string valStr = val;
-      if (valStr.find(argv[1]) == std::string::npos) {
-        valStr += argv[1] + std::string(";");
-        std::cout << "\n\nnew path: " << valStr << endl;
-        // make it set the value of a certain lpValue, (not lpKey!)
-        // you can specify a specific key with a handle obtained from
-        // RegOpenKeyEx() which takes a pointer to a variable that receives a
-        // handle to the opened key
-        LSTATUS RegSetValueExASuccess =
-            RegSetValueExA(hkey, "Path", 0, REG_SZ, (const BYTE*)valStr.c_str(),
-                           valStr.size() + 1) == ERROR_SUCCESS;
-
-        RegCloseKey(HKEY_CURRENT_USER);
-        if (!RegSetValueExASuccess) {
-          logPause("could not setValue");
+      int&& startPos = valStr.find(argv[1]);
+      if (argc == 3) {
+        if (startPos != std::string::npos) {
+          //  include semicolon in the part to be removed
+          valStr.erase(startPos, valStr.length() + 1);
         }
+      } else {
+        if (startPos == std::string::npos) valStr += argv[1] + std::string(";");
+        //  make it set the value of a certain lpValue, (not lpKey!)
+        //  you can specify a specific key with a handle obtained from
+        //  RegOpenKeyEx() which takes a pointer to a variable that later
+        //  receives a handle to the opened key
+      };
+      LSTATUS RegSetValueExASuccess =
+          RegSetValueExA(hkey, "Path", 0, REG_SZ, (const BYTE*)valStr.c_str(),
+                         valStr.size() + 1) == ERROR_SUCCESS;
+
+      RegCloseKey(HKEY_CURRENT_USER);
+      if (!RegSetValueExASuccess) {
+        logPause("could not setValue");
       }
 
     } else {
-      cout << GetLastError();
-      cout << "could not getValue";
+      std::cout << GetLastError();
+      logPause("could not getValue");
     };
-
     return 0;
   }
 
-  cout << "make sure you're running this with administrative priveleges or it "
-          "WILL NOT WORK"
-       << endl;
+  std::cout
+      << "make sure you're running this with administrative priveleges or it "
+         "WILL NOT WORK"
+      << std::endl;
 
   std::string data = std::move(argv[0]);
   data.append(" \"%1\"");
 
-  std::string dirPath = "Folder\\shell\\Add Folder to PATH\\command";
-
+  // prefix both with the same thing so they end up grouped together
+  std::string dirPathAdd = "Folder\\shell\\CP Add Folder to PATH\\command";
+  std::string dirPathRemove =
+      "Folder\\shell\\CP Remove Folder from PATH\\command";
+  // std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
   // dir of type LPCWSTR assigned to (converter.from_bytes(dirPath) cast to a
   // wide string) converted to a const wchar_t*
   // LPCWSTR dir = ((std::wstring)converter.from_bytes(dirPath)).c_str();
 
-  LPCWSTR dir = converter.from_bytes(dirPath).c_str();
+  LPCWSTR dirAdd = std::wstring(dirPathAdd.begin(), dirPathAdd.end()).c_str();
+  LPCWSTR dirRemove =
+      std::wstring(dirPathRemove.begin(), dirPathRemove.end()).c_str();
 
   LPSTR lpData = _strdup(data.c_str());
+  LPSTR lpDataR = _strdup((data + " 1").c_str());
 
-  if (!createKey(HKEY_CLASSES_ROOT,
-                 L"Folder\\shell\\Add Folder to PATH\\command", hkey)) {
+  if (!createKey(HKEY_CLASSES_ROOT, dirAdd, hkey)) {
     logPause("Error during RegOpenKeyW(): ");
     return 1;
   };
 
-  if (!openKey(HKEY_CLASSES_ROOT, L"Folder\\shell\\Add Folder to PATH\\command",
-               KEY_ALL_ACCESS, hkey)) {
+  if (!openKey(HKEY_CLASSES_ROOT, dirAdd, KEY_ALL_ACCESS, hkey)) {
     logPause("Error during RegOpenKeyEx()");
     return 1;
   }
 
-  if (!setValue(HKEY_CLASSES_ROOT, "Folder\\shell\\Add Folder to PATH\\command",
-                REG_SZ, lpData)) {
+  if (!setValue(HKEY_CLASSES_ROOT,
+                "Folder\\shell\\CP Add Folder to PATH\\command", REG_SZ,
+                lpData)) {
+    logPause("Error during RegSetValueA()");
+    return 1;
+  }
+
+  if (RegCloseKey(hkey) != ERROR_SUCCESS) {
+    logPause("Error during RegCloseKey()");
+    return 1;
+  }
+
+  // could use a for loop too
+
+  if (!createKey(HKEY_CLASSES_ROOT, dirRemove, hkey)) {
+    logPause("Error during RegOpenKeyW(): ");
+    return 1;
+  };
+
+  if (!openKey(HKEY_CLASSES_ROOT, dirRemove, KEY_ALL_ACCESS, hkey)) {
+    logPause("Error during RegOpenKeyEx()");
+    return 1;
+  }
+
+  if (!setValue(HKEY_CLASSES_ROOT,
+                "Folder\\shell\\CP Remove Folder from PATH\\command", REG_SZ,
+                lpDataR)) {
     logPause("Error during RegSetValueA()");
     return 1;
   }
